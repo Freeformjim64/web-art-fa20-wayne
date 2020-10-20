80e946e1b659be4ded70ff031083eec0a0e35c55
Freeformjim64 <70674033+Freeformjim64@users.noreply.github.com>
2020-10-19 21:16:50 -0400

    shit

--- JAVASCRIPT_TEST/main.js
+++ JAVASCRIPT_TEST/main.js
@@ -12,5 +12,13 @@ getElementById("box");
 myCoolBox.addEventListener("click", function(){
 
  console.log("I was clicked!");
-
+ printHello()
 })
+
+function printHello() {
+    console.log("Hello");
+}
+
+function addNumbers(num1, num2) {
+    console.log(num1+num2);
+}
