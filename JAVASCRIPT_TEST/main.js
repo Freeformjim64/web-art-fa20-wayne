b184254e741e2c309f75bb9e54cf0d1bd496a1a0
Freeformjim64 <70674033+Freeformjim64@users.noreply.github.com>
2020-11-03 00:45:30 -0500

    new proposal

--- JAVASCRIPT_TEST/main.js
+++ JAVASCRIPT_TEST/main.js
@@ -2,23 +2,38 @@ $(document).ready(function(){
 
   $("#box").on("click", function(){
       console.log("I was clicked! Fuck.");
+      addNumbers(10, 20);
+      console.log(10 + 30);
+      printHello()
+      $('body').css('background', 'purple');
   })
-
-})
-
-let myCoolBox = document.
-getElementById("box");
-
-myCoolBox.addEventListener("click", function(){
-
- console.log("I was clicked!");
- printHello()
-})
 
 function printHello() {
     console.log("Hello");
 }
 
+//Scroll Event
+
+$(document).on('wheel',function(){
+ 
+  let coolBox = '<div id="cool-box"></div>';
+
+  let amountScrolled = $(document).scrollTop()
+
+  console.log("scroll: " + amountScrolled);
+
+  if(amountScrolled > 2500) {
+   $('body').css('background', 'yellow');
+   let p = '<p>Ok dude thats enough like seriously stop</p>';
+
+  }
+ 
+  $('body').append(coolBox);
+
+})
+
+
 function addNumbers(num1, num2) {
-    console.log(num1+num2);
+   console.log(num1 + num2);
 }
+})
